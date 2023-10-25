368352b779a89689a95f4ca83d7d5aaab665121e
KotysCZE <kotyscze@gmail.com>
2023-10-25 18:53:44 +0200

    Updated MVCProjekt 1.2 version

--- MVCProjekt/Program.cs
+++ MVCProjekt/Program.cs
@@ -51,7 +51,7 @@ app.MapControllerRoute(
 //{
 //RoleManager<IdentityRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 //UserManager<IdentityUser> userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-//    IdentityUser? defaultAdminUser = await userManager.FindByEmailAsync("kotyscze@gmail.com");
+//    IdentityUser? defaultAdminUser = await userManager.FindByEmailAsync("");
 
 //    if (!await roleManager.RoleExistsAsync(UserRoles.User))
 //        await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
